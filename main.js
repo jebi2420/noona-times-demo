31ba6de2da95c26ba319861c91606e6c13cf394a
jebi2420 <jebi2420@gmail.com>
2024-02-20 23:02:10 +0900

    [fix] api키 수정

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const API_KEY = '3b7b6f23e8bc401fa30252d944a39d1f';
 const getLatestNews = ()=>{
-    const url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=%${API_KEY}`;
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
     console.log("url: "+ url);
 };
 getLatestNews();
+
