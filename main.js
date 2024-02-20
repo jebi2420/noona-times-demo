1ad90584b2ca3e216326196bda6c2441c846d266
jebi2420 <jebi2420@gmail.com>
2024-02-21 00:07:46 +0900

    [fix] 도메인 주소 변경

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const API_KEY = '3b7b6f23e8bc401fa30252d944a39d1f';
 let news =[]
 const getLatestNews = async () => {
     //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+    const url = new URL(`https://noona-times-demo.netlify.app/top-headlines`);
     console.log("url",url)
     const response =  await fetch(url)
     console.log("response",response)
