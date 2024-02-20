9ece77b8a77cd8aade4e2e2c4dcbfd35cca0a044
jebi2420 <jebi2420@gmail.com>
2024-02-20 23:42:29 +0900

    [add] fetch, await 추가

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 
 const API_KEY = '3b7b6f23e8bc401fa30252d944a39d1f';
-const getLatestNews = () => {
+const getLatestNews = async () => {
     const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
     console.log("url",url)
+    const response =  await fetch(url)
+    console.log("response",response)
+    const data = await response.json();
+    console.log("data",data)
 };
 getLatestNews();
