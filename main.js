9d2c91e49ca5d3e002943c128d901d2d58808fc6
jebi2420 <jebi2420@gmail.com>
2024-02-22 08:53:39 +0900

    [add] 카테고리별 뉴스 보여주기 이벤트

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
 const API_KEY = '3b7b6f23e8bc401fa30252d944a39d1f';
 let newsList =[]
+// 1. 버튼들에 클릭 이벤트 주기
+const menus = document.querySelectorAll(".menus button") // array로 menus 들고 오기
+menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)))
+
 const getLatestNews = async () => {
     //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
     const url = new URL(`https://noona-times-demo.netlify.app/top-headlines`);
@@ -12,6 +16,19 @@ const getLatestNews = async () => {
     render();
     console.log("data",newsList)
 };
+
+// 2. 카테고리별 뉴스 가져오기
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase(); // 이벤트가 발생한 요소 읽어오기
+    console.log("category", category);
+    const url = new URL(`https://noona-times-demo.netlify.app/top-headlines?category=${category}`)
+    const response =  await fetch(url)
+    const data = await response.json();
+    console.log("ddd", data);
+    newsList = data.articles;
+    // 3. 그 뉴스를 보여주기
+    render();
+}
 
 const render = () => {
     const newsHTML = newsList.map(
@@ -38,3 +55,8 @@ const render = () => {
 };
 
 getLatestNews();
+
+const getNewsByKeyword = () => {
+
+}
+
